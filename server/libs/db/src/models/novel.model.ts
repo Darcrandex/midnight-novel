7e205ffe2592ddd6281afb2f256866e5482adcb9
darcrand <darcrandex@gmail.com>
2021-11-04 23:27:29 +0800

    feat:server: 修复 小说 模型

--- server/libs/db/src/models/novel.model.ts
+++ server/libs/db/src/models/novel.model.ts
@@ -1,5 +1,6 @@
 import { Prop, Ref } from '@typegoose/typegoose'
 import { Chapter } from './chapter.model'
+import { SubCategory } from './category.model'
 
 export class Novel {
   @Prop({ required: true })
@@ -11,8 +12,8 @@ export class Novel {
   @Prop()
   cover: string
 
-  @Prop({ type: () => [String] })
-  categories: string[]
+  @Prop({ type: () => [SubCategory], ref: () => SubCategory })
+  categories: Ref<SubCategory>[]
 
   // 章节
   // 这里不需要设置 关联的外键
